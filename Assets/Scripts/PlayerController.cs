74879eeaecd14341e8cc7c1c86e6f9d678675ce7
Kentaro <kentarohakase@tutanota.com>
2023-06-09 12:52:16 +0200

    Update PlayerController
    This script provides basic player control for a character in a 3D game. It allows the player to move, jump, apply gravity, and respawn.
    
    The script utilizes the Unity Character Controller component for character movement and collision detection. It takes input from the player's keyboard or gamepad to control the character's movement.
    
    The player can move forward, backward, left, and right using the WASD or arrow keys. Holding down the Shift key enables sprinting, which increases the player's movement speed. The player can also jump using the spacebar.
    
    The script applies gravity to the player character, causing them to fall towards the ground. The character is grounded when they are standing on a surface, which allows them to jump.
    
    If the player falls below a certain height, defined by the respawnHeight variable, the script will respawn the player at the specified starting position.
    
    The script includes parameters for speed, sprint speed multiplier, jump height, gravity, respawn height, deceleration rate, and acceleration rate. These parameters can be adjusted in the Unity Inspector to customize the player's behavior.
    
    Overall, this script provides a foundation for controlling a player character in a 3D platformer game, allowing for movement, jumping, gravity, and respawning functionality.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,123 +1,90 @@
-using System.Collections;
-using System.Collections.Generic;
-
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
- // Configurable parameters for speed, jump height, gravity, respawn height, acceleration rate, and deceleration rate
- public float speed = 6.0F;
- public float jumpSpeed = 8.0F;
- public float gravity = 20.0F;
- public float respawnHeight = -10.0F;
- public float acceleration = 0.5f;
- public float deceleration = 0.5f;
- 
+ public float speed = 6.0f;  // Geschwindigkeit des Spielers
+ public float sprintSpeedMultiplier = 1.5f;  // Multiplikator für die Sprintgeschwindigkeit
+ public float jumpSpeed = 8.0f;  // Sprunghöhe des Spielers
+ public float gravity = 20.0f;  // Gravitation des Spielers
+ public float respawnHeight = -10.0f;  // Höhe, bei der der Spieler wiederbelebt wird
+ public float deceleration = 0.5f;  // Verzögerung der Geschwindigkeitsabnahme
+ public float acceleration = 0.5f;  // Beschleunigung der Geschwindigkeitszunahme
 
- private Vector3 moveDirection = Vector3.zero;  // The current movement direction
- private Vector3 startPosition;  // The original position for respawning
- private CharacterController controller;  // Reference to the character controller
- private float horizontalInput;  // Current horizontal input (from -1 to 1)
- private float verticalInput;  // Current vertical input (from -1 to 1)
- private float currentSpeed = 0.0f;  // Current speed, will increase with acceleration
+ private CharacterController controller;  // Referenz auf den Character Controller
+ private Vector3 moveDirection = Vector3.zero;  // Aktuelle Bewegungsrichtung des Spielers
+ private bool isGrounded;  // Gibt an, ob der Spieler den Boden berührt
+ private bool isSprinting;  // Gibt an, ob der Spieler sprintet
 
- void Start()
+ private void Start()
  {
-  // Save the starting position and get the character controller
-  startPosition = transform.position;
-  controller = GetComponent<CharacterController>();
+  controller = GetComponent<CharacterController>();  // Character Controller-Komponente abrufen
  }
+
  private void Update()
  {
-  // Check if the player has fallen below the respawn height
-  if ( transform.position.y < respawnHeight )
-  {
-   Respawn();
-  }
- }
- void FixedUpdate()
- {
-  // Update the inputs and handle movement
-  UpdateInput();
-  HandleMovement();
-
-
-
-  // Execute the movement
-  controller.Move( moveDirection * Time.deltaTime );
+  UpdateInput();  // Aktualisiere die Spielersteuerung
+  HandleMovement();  // Behandle die Spielerbewegung
+  ApplyGravity();  // Wende die Gravitation an
+  MovePlayer();  // Bewege den Spieler
+  CheckGrounded();  // Überprüfe, ob der Spieler den Boden berührt
  }
 
  private void UpdateInput()
  {
-  // Retrieve and save the horizontal and vertical input
-  horizontalInput = Input.GetAxis( "Horizontal" );
-  verticalInput = Input.GetAxis( "Vertical" );
+  float horizontalInput = Input.GetAxis( "Horizontal" );  // Horizontaler Eingabewert (-1 bis 1)
+  float verticalInput = Input.GetAxis( "Vertical" );  // Vertikaler Eingabewert (-1 bis 1)
+  isSprinting = Input.GetKey( KeyCode.LeftShift ) || Input.GetKey( KeyCode.RightShift );  // Überprüfe, ob die Shift-Taste gedrückt wird
+
+  Vector3 targetMoveDirection = new Vector3( horizontalInput , 0 , verticalInput );  // Zielflugrichtung basierend auf der Eingabe erstellen
+  targetMoveDirection = transform.TransformDirection( targetMoveDirection );  // Die Flugrichtung entsprechend der Spielerrotation anpassen
+  targetMoveDirection.Normalize();  // Normalisiere die Flugrichtung, um eine gleichmäßige Geschwindigkeit sicherzustellen
+
+  float targetSpeed = speed;  // Zielspeed auf die Standardgeschwindigkeit setzen
+  if ( isSprinting )
+  {
+   targetSpeed *= sprintSpeedMultiplier;  // Wenn der Spieler sprintet, wird die Geschwindigkeit entsprechend des Multiplikators erhöht
+  }
+
+  moveDirection = Vector3.Lerp( moveDirection , targetMoveDirection * targetSpeed , acceleration * Time.deltaTime );  // Bewegungsrichtung mithilfe der Lerp-Funktion allmählich aktualisieren
  }
 
  private void HandleMovement()
  {
-  // Prepare the move if a movement key is pressed
-  if ( horizontalInput != 0 || verticalInput != 0 )
+  if ( isGrounded && Input.GetButton( "Jump" ) )
   {
-   PrepareMove();
+   moveDirection.y = jumpSpeed;  // Wenn der Spieler auf dem Boden ist und die Sprungtaste gedrückt wird, wird eine vertikale Geschwindigkeit für den Sprung hinzugefügt
   }
-  // Decelerate the move if the player is on the ground and no movement key is pressed
-  else if ( controller.isGrounded )
+  else if ( isGrounded )
   {
-   Decelerate();
+   moveDirection.y = 0;  // Wenn der Spieler auf dem Boden ist und keine Bewegungstaste gedrückt wird, wird die vertikale Geschwindigkeit auf 0 gesetzt, um den Spieler stabil zu halten
   }
-
-  // Jump if the player is on the ground and the jump key is pressed
-  if ( controller.isGrounded && Input.GetButton( "Jump" ) )
-  {
-   Jump();
-  }
-
-  // Apply gravity
-  ApplyGravity();
- }
-
- private void PrepareMove()
- {
-  // Calculate the desired movement direction based on player input
-  Vector3 targetMoveDirection = new Vector3( horizontalInput , 0 , verticalInput );
-  targetMoveDirection.Normalize();
-  targetMoveDirection = transform.TransformDirection( targetMoveDirection );
-  targetMoveDirection *= currentSpeed;
-
-  // Increase current speed based on acceleration
-  currentSpeed += acceleration * Time.deltaTime;
-  currentSpeed = Mathf.Min( currentSpeed , speed );  // Cap current speed at the defined speed
-
-  // Update the horizontal and vertical components of the movement direction
-  moveDirection.x = targetMoveDirection.x;
-  moveDirection.z = targetMoveDirection.z;
- }
-
- private void Jump()
- {
-  // Set the vertical component of the movement direction to the jump height
-  moveDirection.y = jumpSpeed;
  }
 
  private void ApplyGravity()
  {
-  // Apply gravity to the vertical component of the movement direction
-  moveDirection.y -= gravity * Time.deltaTime;
+  moveDirection.y -= gravity * Time.deltaTime;  // Gravitation auf die vertikale Geschwindigkeit anwenden, um den Spieler nach unten zu ziehen
+ }
+
+ private void MovePlayer()
+ {
+  controller.Move( moveDirection * Time.deltaTime );  // Den Spieler mithilfe des Character Controllers bewegen
+ }
+
+ private void CheckGrounded()
+ {
+  isGrounded = controller.isGrounded;  // Überprüfen, ob der Spieler den Boden berührt
+
+  if ( isGrounded && moveDirection.y < 0 )
+  {
+   moveDirection.y = -0.5f;  // Eine leichte negative vertikale Geschwindigkeit hinzufügen, um den Spieler auf dem Boden zu halten
+  }
  }
 
  private void Respawn()
  {
-  // Reset the player position to the starting position
-  transform.position = startPosition;
- }
-
- private void Decelerate()
- {
-  // Decelerate the horizontal and vertical components of the movement direction
-  moveDirection.x = Mathf.Lerp( moveDirection.x , 0 , deceleration * Time.deltaTime );
-  moveDirection.z = Mathf.Lerp( moveDirection.z , 0 , deceleration * Time.deltaTime );
-  // Also decelerate the current speed
-  currentSpeed = Mathf.Lerp( currentSpeed , 0 , deceleration * Time.deltaTime );
+  Vector3 startPosition = Vector3.zero;  // Die gewünschte Startposition für die Wiederbelebung des Spielers festlegen (hier: (0, 0, 0))
+  controller.enabled = false;  // Den Character Controller deaktivieren, um Interaktionen zu verhindern
+  transform.position = startPosition;  // Die Position des Spielers auf die Startposition setzen
+  controller.enabled = true;  // Den Character Controller aktivieren, um Interaktionen wieder zu ermöglichen
  }
 }
